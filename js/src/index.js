83562396a84673752ca7faf7845ca8dbd1925ae1
TashimaTakumi <b1015103@fun.ac.jp>
2018-01-26 21:35:48 +0900

    change program to be able to view modal window

--- js/src/index.js
+++ js/src/index.js
@@ -4,7 +4,7 @@ window.onload=function(){
   for(let i=1;i<=43;i++){
     //console.log(`loop was started. now number is ${i}`);
     let oneLi=`<li><section><figure><img onclick="modalOn(this)" src="img/domesticOpen/domeOpen(${i}).jpg"/></figure></section></li>`;
-    console.log(parseInt(`img/domesticOpen/domeOpen(${i}).jpg`));
+    //console.log(`img/domesticOpen/domeOpen(${i}).jpg`.replace(/[^0-9]/g,""));
     result+=oneLi;
   }
   //console.log(`result is here:${result}`);
@@ -12,14 +12,42 @@ window.onload=function(){
 }
 
 modalOn=function(e){
-  modalWindow.opacity="1";
-  modalWindow.visibility="visible";
+  console.log("modalOpen");
+  modalWindow.style.opacity="1";
+  modalWindow.style.visibility="visible";
+  blackWindow.style.opacity="0.95";
+  blackWindow.style.visibility="visible";
+  console.log(e.src);
   modalImg.src=e.src;
+  modalWindow.style.height=modaImg.height;
+  modalWindow.style.width=modalImg.width;
 }
-goToNextMmodalWindow=function(){
+goToNextModalWindow=function(){
+  //console.log("goToNext!")
   const nowSrc=modalImg.src;
-  //const nowNumber=nowSrc.slice()
+  const nowNumber=parseInt(nowSrc.replace(/[^0-9]/g,""));
+  console.log(nowNumber);
+  if(nowNumber<43){
+    modalImg.src=`img/domesticOpen/domeOpen(${nowNumber+1}).jpg`;
+  }
+  modalWindow.height=modaImg.height;
+  modalWindow.width=modalImg.width;
+}
+goToBeforeModalWindow=function(){
+  const nowSrc=modalImg.src;
+  const nowNumber=nowSrc.replace(/[^0-9]/g,"");
+  if(nowNumber>1){
+    modalImg.src=`img/domesticOpen/domeOpen(${nowNumber-1}).jpg`
+  }
+  modalWindow.style.height=modaImg.height;
+  modalWindow.style.width=modalImg.width;
 }
 modalOff=function(){
-  modalWindow.opacity="0";
+  modalWindow.style.opacity="0";
+
+  blackWindow.style.opacity="0";
+  blackWindow.style.visibility="hidden";
+  setTimeout(()=>{
+      modalWindow.style.visibility="hidden";
+  },0.3*1000)
 }
